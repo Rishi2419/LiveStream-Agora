94d2002a9a3a8df343b68108be289d13821763ca
Rishi2419 <sahurishikesh1908@gmail.com>
2025-03-13 20:09:01 +0530

    ui

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
     implementation("io.agora.rtc:full-sdk:4.0.1")
+    implementation ("com.airbnb.android:lottie:6.0.0")
+
 
 
     implementation ("androidx.camera:camera-core:1.2.3")
